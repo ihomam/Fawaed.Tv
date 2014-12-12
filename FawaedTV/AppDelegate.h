bdc615b4b991a0dac73a1530a93332f4e0d64885
Homam Mawaldi <ihomam.m@gmail.com>
2014-12-12 23:54:10 +0100

    Initial Commit

---
+++ FawaedTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FawaedTV
+//
+//  Created by Homam on 2014-12-12.
+//  Copyright (c) 2014 Homam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
