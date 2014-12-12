2c0a4abc88eede941f6935e18604305d0ff35db7
Homam Mawaldi <ihomam.m@gmail.com>
2014-12-13 00:05:13 +0100

    adding afnetworking framework

---
+++ FawaedTV/serverObject.h
@@ -0,0 +1,13 @@
+//
+//  serverObject.h
+//  FawaedTV
+//
+//  Created by Homam on 2014-12-12.
+//  Copyright (c) 2014 Homam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface serverObject : NSObject
+
+@end
---
+++ FawaedTV/serverObject.m
@@ -0,0 +1,13 @@
+//
+//  serverObject.m
+//  FawaedTV
+//
+//  Created by Homam on 2014-12-12.
+//  Copyright (c) 2014 Homam. All rights reserved.
+//
+
+#import "serverObject.h"
+
+@implementation serverObject
+
+@end
